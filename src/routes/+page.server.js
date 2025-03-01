721817e08f17d522c7ad791fa72bc2befd742df6
adaotilia <159614421+adaotilia@users.noreply.github.com>
2025-03-01 11:52:42 +0100

    git push

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,13 @@
-export const load = async ({event}) =>{
-    console.log(event)
+export const load = async (event) => {
+    console.log(event.locals)
     return {
-        user: event.locals.user
+        user: event.locals.user,
+        cica: 23264
+    }
+}
+export const actions = {
+    default: async (event) => {
+        var data = await event.request.fromDat();
+        console.log(data.get('cica'));
     }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script>
+    const {data} = $props()
+    console.log(data)
+</script>
 <h1>Cica</h1>
+{data.user.username}
+<form method="POST">
+    <div class="ui input">
+        <input type="text" name="cica">
+    </div>
+</form>
